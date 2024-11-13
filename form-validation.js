25f50bbccd5b95010130f114ac980490785365ac
McKathlin <mckathlin@gmail.com>
2024-11-13 19:11:50 +0000

    Make invalid inputs show red border after submit

--- form-validation.js
+++ form-validation.js
@@ -1,1 +1,13 @@
-console.log("The script is loaded!");
+const formNode = document.getElementById('sign-up-form');
+const signInInputsNode = document.getElementById('sign-up-inputs');
+
+formNode.addEventListener("submit", (event) => {
+    if (!formNode.checkValidity()) {
+        event.preventDefault();
+    }
+    
+
+    if (!signInInputsNode.classList.contains("post-submit")) {
+        signInInputsNode.classList.add("post-submit");
+    }
+})
